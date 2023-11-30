e84cf9d5334cfceacee6f3a4683ce59d60d3561f
Ledo Ivo Veras <ledoivoveras@gmail.com>
2023-11-30 02:30:37 -0300

    navbar

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var loginLogoutLink = document.getElementById("loginLogout");
+    
+        if (currentAccount === null) {
+          // Se a conta for nula, exibe "Login"
+          loginLogoutLink.innerHTML = "Login";
+        } else {
+          // Se a conta não for nula, exibe "Logout"
+          loginLogoutLink.innerHTML = "Logout";
+        }
